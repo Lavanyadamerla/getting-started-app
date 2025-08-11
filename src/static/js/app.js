cecd402a0e54ed4ae68371cd226f5b2fb3b4db7a
Lavanyadamerla <darmla.lavanya@gmail.com>
2025-08-12 03:51:47 +0530

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -107,7 +107,6 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        // {submitting ? 'Adding...' : 'Add Item'}
                         {submitting ? 'Adding...' : 'Add'}
                     </Button>
                 </InputGroup.Append>
